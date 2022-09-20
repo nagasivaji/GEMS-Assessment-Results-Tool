b93ff5407ad6c076649dd07beb3aded20376a3b2
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-20 13:25:54 +0530

    create input area

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-// //Accessing Process Button
-// const processFileBtn = document.getElementById("processFileBtn");
-// // Accessing file input
-// const excelFile = document.getElementById('excelFile');
+//Accessing Process Button
+const processFileBtn = document.getElementById("processFileBtn");
+// Accessing file input
+const excelFile = document.getElementById('excelFile');
 
 
 
-// // Adding event Listener to handle click evenr
-// processFileBtn.addEventListener("click", () =>{
-//     processExcelFile();
-// });
+// Adding event Listener to handle click evenr
+processFileBtn.addEventListener("click", () =>{
+    processExcelFile();
+});
 
 
 
-// // Variables
+// Variables
 
 
 
-// function processExcelFile(){
-//     //console.log("Processing");
+function processExcelFile(){
+    //console.log("Processing");
 
-//     try {
-//         if(checkExcelOrNot()){
-//             console.log("Correct File...");
-//         }else{
-//             console.log("Wrong File...");
-//             const errorArea = document.getElementById("errorArea");
-//             errorArea.style.display = 'block';
-//             errorArea.innerHTML = "<p>Only Excel files are allowed</p>";
-//             setTimeout(() => {
-//                 location.reload();
-//             }, 2000);
-//         }
-//     } catch (e) {
-//         const errorArea = document.getElementById("errorArea");
-//         errorArea.style.display = 'block';
-//         errorArea.innerHTML = "<p>No file Choosen</p>";
-//         setTimeout(() => {
-//             location.reload();
-//         }, 2000);
-//     }
+    try {
+        if(checkExcelOrNot()){
+            console.log("Correct File...");
+        }else{
+            console.log("Wrong File...");
+            const errorArea = document.getElementById("errorArea");
+            errorArea.style.display = 'block';
+            errorArea.innerHTML = "<p>Only Excel files are allowed</p>";
+            setTimeout(() => {
+                location.reload();
+            }, 2000);
+        }
+    } catch (e) {
+        const errorArea = document.getElementById("errorArea");
+        errorArea.style.display = 'block';
+        errorArea.innerHTML = "<p>No file Choosen</p>";
+        setTimeout(() => {
+            location.reload();
+        }, 2000);
+    }
     
-// }
+}
 
 
 
-// // file type checker
-// function checkExcelOrNot() {
+// file type checker
+function checkExcelOrNot() {
 
-//     if (!['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-//             'application/vnd.ms-excel'
-//         ].includes(excelFile.files[0].type)) {
+    if (!['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'application/vnd.ms-excel'
+        ].includes(excelFile.files[0].type)) {
 
-//         //excelFile.value = '';
+        //excelFile.value = '';
 
-//         return false;
-//     }
-//     return true;
-// }
+        return false;
+    }
+    return true;
+}
